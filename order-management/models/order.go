d99a7f3504b97921f3993cf8ec332503051e844b
thony-obser-plenty <thony.obser@plentysystems.com>
2023-10-06 13:55:07 +0200

    feat(database-gorm): created-models
    - Added order model
    - Added order item model
    - Configurated models for use with gorm orm

---
+++ order-management/models/order.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type Order struct {
+	gorm.Model
+	ID              uint      `gorm:"primaryKey"`
+	CustomerID      int       `gorm:"type:int"`
+	OrderNumber     string    `gorm:"type:varchar(255)"`
+	OrderDate       time.Time `gorm:"type:datetime"`
+	Status          string    `gorm:"type:varchar(50)"`
+	TotalSum        float64   `gorm:"type:decimal(10,2)"`
+	BillingAddress  string    `gorm:"type:varchar(255)"`
+	ShippingAddress string    `gorm:"type:varchar(255)"`
+	OrderItems      []OrderItem
+}
---
+++ order-management/models/order_item.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type OrderItem struct {
+	gorm.Model
+	ID       uint    `gorm:"primaryKey"`
+	OrderID  uint    `gorm:"type:int"`
+	OfferID  uint    `gorm:"type:int"`
+	GTIN     string  `gorm:"type:varchar(20)"`
+	Price    float64 `gorm:"type:decimal(10,2)"`
+	Quantity int     `gorm:"type:int"`
+	SKU      string  `gorm:"type:varchar(255)"`
+}
