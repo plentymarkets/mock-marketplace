246b0bd018887fa605b5afccf005bf0da5203488
laurentiuvdorin <dorineltuta2006@yahoo.com>
2023-10-26 15:20:32 +0300

    Product Backlog Item 78085: [Mock Marketplace] API for Offer Management - Fixed Database Product search

--- product-management/pkg/repositories/product_repository_contract.go
+++ product-management/pkg/repositories/product_repository_contract.go
@@ -6,6 +6,7 @@ import (
 
 type ProductRepositoryContract interface {
 	FetchByID(id string) (models.Product, error)
+	FetchByProduct(product models.Product) (models.Product, error)
 	FetchAll(page int, productsPerPage int) ([]models.Product, int, error)
 	Create(product models.Product) (models.Product, error)
 	Update(product models.Product) (models.Product, error)
