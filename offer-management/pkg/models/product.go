a8409e13eb3d37fc28f841bfce577001db59e502
thony-obser-plenty <thony.obser@plentysystems.com>
2023-11-13 13:44:18 +0100

    refactor(offer-microservice): Removal
    - Removed offer management to build a new version from scratch.

--- offer-management/pkg/models/product.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Product struct {
-	ID     uint   `gorm:"primarykey"`
-	Name   string `gorm:"<-" json:"name" binding:"required"`
-	GTIN   string `gorm:"<-" json:"gtin" binding:"required"`
-	SKU    string `gorm:"<-" json:"sku"`
-	Offers []Offer
-}
